f47dabac941def71b104d3d7ac1df46bd94025e6
khushboo saini <khushboo30102005@gmail.com>
2026-01-29 19:32:56 +0530

    Explore process in node.js

---
+++ Section-3/01_Process/process.js
@@ -0,0 +1,26 @@
+// What is Process ==>
+// Process is a computer application or a program that take some memory and computational power to executes and processor Execute it.
+
+// States of a Process -->
+// 1. Ready State
+// 2. Waiting State
+// 3. Running State
+// 4. Terminated State
+
+// 1. Ready State -->
+// In this state, the process is loaded into the main memory and is waiting for the CPU to be allocated to it. The process is ready to execute but is not currently being executed.
+// Example: When you open a text editor, it is in the ready state until the CPU allocates time to it.
+
+// 2. Waiting State -->
+// In this state, the process is waiting for some event to occur or for some resource to become available. The process cannot proceed until the event occurs or the resource is available.
+// Example: A process waiting for user input or waiting for a file to be read from disk.
+
+// 3. Running State -->
+// In this state, the process is currently being executed by the CPU. The process has been allocated CPU time and is actively performing its tasks.
+// Example: When you are typing in the text editor, the process is in the running state.
+
+// 4. Terminated State -->
+// In this state, the process has completed its execution and is no longer active. The resources allocated to the process are released back to the system.
+// Example: When you close the text editor after saving your work, the process enters the terminated state.
+
+// check more about process in system informer and task manager.
---
+++ Section-3/01_Process/script.js
@@ -0,0 +1,8 @@
+// setInterval(() => {
+//   console.log('hello');
+// }, 1000);
+
+// process is a global Node.js object that provides information about the current Node process.
+const a = process
+console.log(a);
+console.log('end');

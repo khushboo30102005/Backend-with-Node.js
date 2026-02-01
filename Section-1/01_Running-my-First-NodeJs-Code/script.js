d9d5d436703f6b38e8bb1c73de27bf13528a7323
khushboo saini <khushboo30102005@gmail.com>
2026-02-01 13:00:05 +0530

    Remove duplicate Section-1 folder

--- Section-1/01_Running-my-First-NodeJs-Code/script.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs")
-const text = fs.readFileSync("C:\\Users\\T14\\Desktop\\hello.txt")
-console.log(text.toString());
-console.log(global);
-console.log("END");
--- Section-1/02_Why-NodeJs/server.js
+++
@@ -1,5 +0,0 @@
-const http = require("http")
-const server = http.createServer((req, res) => {
-  res.end("Hello form node.js server....")
-})
-server.listen(3000)

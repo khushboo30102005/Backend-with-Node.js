cbeb1b98ec7e3e3a57d38e032b2a1d30139967f7
Khushboo Saini <khushboo30102005@gmail.com>
2026-03-10 12:47:35 +0530

    Practical

---
+++ Section-06_Mastering-Buffers-Handling-Binary-Data-in-Node.js/11_Practical-Use-of_Buffers/app.js
@@ -0,0 +1,4 @@
+// import fs from 'node:fs/promises'
+// // const a = await fs.readFile('file.txt')
+// const a = await fs.readFile("C:\\Users\\T14\\Desktop\\ChatGPT Image Feb 24, 2026, 09_37_35 PM.png")
+// console.log(a.byteLength)

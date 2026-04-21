492932de8a22c36ff01c578ace764863cd51676c
Khushboo Saini <khushboo30102005@gmail.com>
2026-04-21 15:07:03 +0530

    Adding Multi-Level Nested Directory Support

--- File-Storage-Project/app.js
+++ File-Storage-Project/app.js
@@ -1,80 +1,79 @@
 import express from 'express';
 import { createWriteStream } from 'fs';
-import { open, readdir, rename, rm } from 'fs/promises';
+import { open, readdir, rename, rm, stat } from 'fs/promises';
+import cors from 'cors';
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 const port = 4000;
 
 app.use(express.json());
-// Enable CORS
-app.use((req, res, next) => {
-  res.set({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*',
-  });
-  next();
+
+app.get('/directory/{*splat}', async (req, res) => {
+  const splat = req.params.splat?.join('/');
+  const filesList = await readdir(`./public${splat ? `/${splat}` : ''}`);
+  const resData = [];
+  for (const item of filesList) {
+    const stats = await stat(`./public${splat ? `/${splat}` : ''}/${item}`);
+    resData.push({
+      name: item,
+      isDirectory: stats.isDirectory(),
+    });
+  }
+  res.json(resData);
 });
-
-// app.use(express.static('public'));
-// This code is equals to :
-
-// app.use((req, res, next) => {
-//   express.static('public')(req, res, next)
-// })
-
-//  now we can :
-
-// Serve Dir Content
-app.get('/', async (req, res) => {
-  const publicContent = await readdir('./public');
-  res.json(publicContent);
-});
-
 
 // Upload a file: CREATE
-app.post('/:filename', async (req, res) => {
-  const { filename } = req.params;
-  const writeStream = createWriteStream(`./public/${filename}`);
+app.post('/files/*splat', async (req, res) => {
+  const filePath = req.params.splat.join('/');
+  const writeStream = createWriteStream(`./public/${filePath}`);
   req.pipe(writeStream);
   req.on('end', () => {
     res.json({ message: 'File Uploaded Successfully' });
   });
 });
 
-
-// Dynamic Routing : READ
-app.get('/:filename', (req, res, next) => {
-  if (req.query.action === 'download') {
-    res.set('Content-Disposition', 'attachment');
+// Dynamic Routing : READ {.:ext}
+app.get('/files/*splat', async (req, res, next) => {
+  const filePath = req.params.splat.join('/');
+  console.log(filePath);
+  try {
+    const { filename } = req.params;
+    if (req.query.action === 'download') {
+      res.set('Content-Disposition', 'attachment');
+    }
+    res.sendFile(`${import.meta.dirname}/public/${filePath}`);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
-  const { filename } = req.params;
-  res.sendFile(`${import.meta.dirname}/public/${filename}`);
 });
 
-
 // Rename a File: UPDATE
-app.patch('/:oldFilename', async (req, res) => {
+app.patch('/files/*splat', async (req, res) => {
+  const oldFilePath = req.params.splat.join('/');
+  // const newFilePath = req.params.splat.slice(0, -1).join('/') + '/' + req.body.newFilename;
+
   try {
-    const { oldFilename } = req.params;
-    console.log(oldFilename);
-    await rename(`public/${oldFilename}`, `public/${req.body.newFilename}`);
+    const { filename } = req.params;
+    await rename(`public/${oldFilePath}`, `public/${req.body.newFilename}`);
     res.json({ message: 'Renamed Successfully' });
   } catch (error) {
     res.json({ message: error.message });
   }
 });
 
-
 // handling deletion : DELETE
-app.delete('/:filename', async (req, res) => {
-  const { filename } = req.params;
+app.delete('/files/{*splat}', async (req, res) => {
+  const splat = req.params.splat?.join('/');
+  console.log(splat)
   try {
-    await rm(`public/${filename}`);
-    res.json({message: 'File Deleted Successfully'});
+    await rm(`public/${splat}`,  { recursive: true, force: true });
+    res.json({ message: 'File Deleted Successfully' });
   } catch (error) {
-    res.status(404).json({ message: 'no such file or directory' });
+    res.status(404).json({ message: error.message });
   }
 });
 

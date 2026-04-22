294696958dfeee32f3464de1e402f236187a1a48
Khushboo Saini <khushboo30102005@gmail.com>
2026-04-22 09:51:53 +0530

    Audit: Found Path Traversal vulnerabilities in file system routes

--- File-Storage-Project/app.js
+++ File-Storage-Project/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { createWriteStream } from 'fs';
-import { open, readdir, rename, rm, stat } from 'fs/promises';
+import { createReadStream, createWriteStream, read } from 'fs';
+import { mkdir, open, readdir, rename, rm, stat } from 'fs/promises';
 import cors from 'cors';
 
 const app = express();
@@ -11,6 +11,8 @@ app.use(cors());
 const port = 4000;
 
 app.use(express.json());
+
+// Path Traversal Vulnerability detection (A Dangerous Security Risk, Not Recommended)
 
 app.get('/directory/{*splat}', async (req, res) => {
   const splat = req.params.splat?.join('/');
@@ -26,8 +28,28 @@ app.get('/directory/{*splat}', async (req, res) => {
   res.json(resData);
 });
 
+
+// Create a directory: CREATE
+app.post('/directory/{*splat}', async (req, res) => {
+  console.log(req.params.splat?.slice(-1));
+  const splat = req.params.splat?.join('/');
+  const isDirName = req.params.splat?.slice(-1)[0] == '' ? false : true;
+  console.log(req.params.splat?.slice(-1), { isDirName });
+  console.log(splat);
+  try {
+    if (isDirName) {
+      await mkdir(`./public${splat ? `/${splat}` : ''}`, { recursive: true });
+      res.json({ message: 'Directory Created Successfully' });
+    } else {
+      res.status(400).json({ message: 'Directory name is required' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Upload a file: CREATE
-app.post('/files/*splat', async (req, res) => {
+app.post('/files/{*splat}', async (req, res) => {
   const filePath = req.params.splat.join('/');
   const writeStream = createWriteStream(`./public/${filePath}`);
   req.pipe(writeStream);
@@ -45,7 +67,9 @@ app.get('/files/*splat', async (req, res, next) => {
     if (req.query.action === 'download') {
       res.set('Content-Disposition', 'attachment');
     }
-    res.sendFile(`${import.meta.dirname}/public/${filePath}`);
+    // res.sendFile(`${import.meta.dirname}/public/${filePath}`);
+    const readStream = createReadStream((`${import.meta.dirname}/public/${filePath}`));
+    readStream.pipe(res);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -68,9 +92,9 @@ app.patch('/files/*splat', async (req, res) => {
 // handling deletion : DELETE
 app.delete('/files/{*splat}', async (req, res) => {
   const splat = req.params.splat?.join('/');
-  console.log(splat)
+  console.log(splat);
   try {
-    await rm(`public/${splat}`,  { recursive: true, force: true });
+    await rm(`public/${splat}`, { recursive: true, force: true });
     res.json({ message: 'File Deleted Successfully' });
   } catch (error) {
     res.status(404).json({ message: error.message });

ce36f1ff4d8690703f16b660524dd1fe5c10273c
Khushboo Saini <khushboo30102005@gmail.com>
2026-06-30 19:35:32 +0530

    How mac and hmac differs from signature

---
+++ Section-14_Mastering Authentication and Authorization/08_understanding-mac-and-hmac/app.mjs
@@ -0,0 +1,10 @@
+import crypto from "node:crypto";
+import { readFile } from "node:fs/promises";
+
+const fileContent = await readFile("mac.md");
+const mySecretKey = "my-super-secret-key";
+
+const hmac = crypto.createHmac('sha256', mySecretKey).update(fileContent).digest('hex')
+const simpleHmac = crypto.createHash('sha256').update(fileContent).update(mySecretKey).digest('hex')
+console.log(hmac)
+console.log(simpleHmac)

292b12a06a26f0737ded11b5f1b4cdadb1c4b095
Khushboo Saini <khushboo30102005@gmail.com>
2026-08-11 13:20:29 +0530

    Keys vs scan command

---
+++ Section-16_Redis-Essentials-for-Performant-Applications/13_Understanding-SCAN-Command-in-Redis/app.mjs
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+
+await redisClient.connect();
+
+let cursor = '0';
+
+do {
+  const { cursor: nextCursor, keys } = await redisClient.scan(cursor, {
+    MATCH: '*',
+
+    COUNT: 20,
+  });
+
+  console.log(nextCursor);
+
+  console.log(keys);
+
+  cursor = nextCursor;
+} while (cursor !== '0');
+
+await redisClient.quit();
